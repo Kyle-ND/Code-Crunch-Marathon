65ed2ce6da372bfe886e88f0336b847879aad42f
FreakyJay1 <jjthebestmanmode@gmail.com>
2024-10-03 10:27:38 +0200

    second exercise

--- practice.py
+++ practice.py
@@ -23,6 +23,9 @@ list_nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 student_names = ["jason", "james", "jane", "jill", "jerry"]
 
-for student,value in students.items():
-    if value > 50:
-        print(student)
+# for student,value in students.items():
+#     if value > 50:
+#         print(student)
+        
+high = students["jason"]
+print(high)
