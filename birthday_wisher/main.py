35620016a68676180f31b284366eed889af38d22
Auston Mtabane <mtabane.auston@gmail.com>
2024-10-21 01:22:14 +0200

    api setup and data fetch

--- birthday_wisher/main.py
+++ birthday_wisher/main.py
@@ -1,6 +1,7 @@
 import smtplib
 import datetime
 from email.message import EmailMessage
+import requests,json
 
 def send_email(receipient_data):
     email = EmailMessage()
@@ -9,7 +10,18 @@ def send_email(receipient_data):
     email['To'] = ''
     email.set_charset('')
     
+    
+def get_data():
+    End_point= "https://api.sheety.co/ef491bcfd06cc17c7074d5b4b6f10bbc/birthdaySheet/dateofbirths"
+    
+    response = requests.get(url=End_point)
+    str1 = response.text
+    data  = json.loads(str1)
+    
+    return data['dateofbirths']
 
 
-date = datetime.datetime.now()
-print(date)
+#response = requests.post(url=End_point,json=user_info)
+print(get_data())
+# date = datetime.datetime.now()
+# print(date)
