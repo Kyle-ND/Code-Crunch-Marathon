700f7bc215346164dbcf43cdee22369ed34db068
Auston Mtabane <mtabane.auston@gmail.com>
2024-10-20 23:33:37 +0200

    added send_email func

--- birthday_wisher/main.py
+++ birthday_wisher/main.py
@@ -2,5 +2,14 @@ import smtplib
 import datetime
 from email.message import EmailMessage
 
+def send_email(receipient_data):
+    email = EmailMessage()
+    email['Subject'] = 'Happy Birthday 🎂'
+    email['From'] = 'Automate test'
+    email['To'] = ''
+    email.set_charset('')
+    
+
+
 date = datetime.datetime.now()
 print(date)
