65021ef18692f78f76abbea42002daea41c574dc
Auston Mtabane <mtabane.auston@gmail.com>
2024-10-21 19:32:02 +0200

    added more user input options

--- birthday_wisher/main.py
+++ birthday_wisher/main.py
@@ -2,6 +2,7 @@ import smtplib
 import datetime
 from email.message import EmailMessage
 import requests,json
+from menu import option_sys
 
 def send_email(person,age): #will send message via email to a certain address. 
     name = person['name']
@@ -41,19 +42,42 @@ def get_data():
 
 
 def main():
-    
+
     people = get_data()
     date = datetime.datetime.now()
     day,month,year = date.strftime('%d'), date.strftime('%m'), date.strftime('%Y')
+    
+    options = ["View Todays birthday","View ALL Birthdays","Add A Birthday (+)","Remove A Birthday (-)"] 
+    while True:
+        choice = option_sys(options,"============== Birthday Wisher Deshboard: ==============\n \n (press 'p' to select)\n press ENTER to move through options")
+        is_a_birthday_today = False
+        match choice:
+            case 0:
+                print("\033[1;34m",options[choice],"\033[0;37m")
+                for person in people:
+                    
+                    d_o_b = person['dateOfBirth'].split("/")
+                    if d_o_b[0] == day and d_o_b[1] == month: #
+                        age = int(year)-int(d_o_b[2])
+                        send_email(person,age)
+                        is_a_birthday_today = True
+                        
+                
+            case 1:
+                print("\033[1;34m",options[choice],"\033[0;37m")
+                for pp in people:
+                    print("-> ",pp['dateOfBirth'],pp['name'])
 
-    for person in people:
-        d_o_b = person['dateOfBirth'].split("/")
-        if d_o_b[0] == day and d_o_b[1] == month: #
-            age = int(year)-int(d_o_b[2])
-            send_email(person,age)
-        else:
-            pass
+            case 2:
+                print("\033[1;34m",options[choice],"\033[0;37m")
+                pass
+            case 3:
+                print("\033[1;34m",options[choice],"\033[0;37m")
+                
+                pass
         
+        input("")
+            
 
  
 if __name__ == '__main__':
