96cb9b76822b0c5416c68bac6fb5c4b36e416528
Auston Mtabane <mtabane.auston@gmail.com>
2024-10-21 20:42:47 +0200

    added add_birthday() and (view all birthdays)

--- birthday_wisher/main.py
+++ birthday_wisher/main.py
@@ -37,18 +37,37 @@ def get_data():
     str1 = response.text
     data  = json.loads(str1)
     
+    
     return data['dateofbirths']
 
+def add_birthday(name,surname,dateofbirth,email):
+    End_point= "https://api.sheety.co/ef491bcfd06cc17c7074d5b4b6f10bbc/birthdaySheet/dateofbirths"
+    
+    data = {
+        "dateofbirth": {
+            "name" : name,
+            "surname" : surname,
+            "dateOfBirth" : dateofbirth,
+            "email": email
+
+        }
+    }
+
+    response = requests.post(url=End_point,json=data)
+    response.raise_for_status()
 
 
 def main():
 
-    people = get_data()
-    date = datetime.datetime.now()
-    day,month,year = date.strftime('%d'), date.strftime('%m'), date.strftime('%Y')
     
+
     options = ["View Todays birthday","View ALL Birthdays","Add A Birthday (+)","Remove A Birthday (-)"] 
     while True:
+        people = get_data()
+        date = datetime.datetime.now()
+        day,month,year = date.strftime('%d'), date.strftime('%m'), date.strftime('%Y')
+        
+        
         choice = option_sys(options,"============== Birthday Wisher Deshboard: ==============\n \n (press 'p' to select)\n press ENTER to move through options")
         is_a_birthday_today = False
         match choice:
@@ -57,21 +76,32 @@ def main():
                 for person in people:
                     
                     d_o_b = person['dateOfBirth'].split("/")
-                    if d_o_b[0] == day and d_o_b[1] == month: #
+                    if d_o_b[0] == day and d_o_b[1] == month: #send email if today is a birthday 
                         age = int(year)-int(d_o_b[2])
                         send_email(person,age)
                         is_a_birthday_today = True
                         
+                        
+                if is_a_birthday_today is not True:
+                    print("There is no birthday Today...🚫")
+                        
                 
-            case 1:
+            case 1:# view all birthdays
                 print("\033[1;34m",options[choice],"\033[0;37m")
                 for pp in people:
                     print("-> ",pp['dateOfBirth'],pp['name'])
 
-            case 2:
+            case 2:# add
                 print("\033[1;34m",options[choice],"\033[0;37m")
+                name = input("\033[1;34mName: \033[0;37m")
+                surname = input("\033[1;34mSurname: \033[0;37m")
+                dateofbirth = input("\033[1;34mDOB [dd/mm/yyyy]: \033[0;37m")
+                email = input("\033[1;34mEmail: \033[0;37m")
+            
+                add_birthday(name,surname,dateofbirth,email)
+                
                 pass
-            case 3:
+            case 3:#remove
                 print("\033[1;34m",options[choice],"\033[0;37m")
                 
                 pass
