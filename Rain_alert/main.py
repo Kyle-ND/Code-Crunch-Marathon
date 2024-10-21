6e9de137b7d66850a8909186047f31bb6191ff57
Thato Mabuela <mabuelathato03@gmail.com>
2024-10-21 21:56:06 +0200

    Add weather condition check

--- Rain_alert/main.py
+++ Rain_alert/main.py
@@ -1,28 +1,32 @@
-#import geocoder
+import geocoder
 import requests
 
-# def get_coordinates():
-#     g = geocoder.ip("me")
+def get_coordinates():
+    g = geocoder.ip("me")
 
-#     if g.latlng is not None:
-#         return g.latlng
-#     else:
-#         return None
+    if g.latlng is not None:
+        return g.latlng
+    else:
+        return None
 
 
 def main():
-    # coordinates = get_coordinates()
-    # if coordinates is not None:
-    #     latitude, longitude = coordinates
-    # else:
-    #     print("Unable to retrieve your GPS coordinates.")
+    coordinates = get_coordinates()
+    if coordinates is not None:
+        latitude, longitude = coordinates
+    else:
+        print("Unable to retrieve your GPS coordinates.")
     
-    key = "c61cc657a57e7c94cb7c0f3dc790a008"
+    key = "0c1610760b9e94815ac1574e5980ed3a"
     url = f"https://api.openweathermap.org/data/3.0/onecall?lat={latitude}&lon={longitude}&exclude=hourly,daily&appid={key}"
     response = requests.get(url).json()
-    rainy = response["current"]["rain"]
 
-    print(response)
+    rainy = response["current"]["weather"][0]["main"]
+
+    if rainy == "rain":
+        print("Send message")
+    else:
+        print("Dont send message")
 
 if __name__ == "__main__":
     main()
