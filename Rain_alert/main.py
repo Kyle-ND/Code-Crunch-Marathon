48bf1d03dd90251788c58a77afe48a3b6c6621a6
Anele Nenngwekhulu <annenngjhb024@student.wethinkcode.co.za>
2024-10-24 09:48:19 +0200

    update

--- Rain_alert/main.py
+++ Rain_alert/main.py
@@ -3,17 +3,13 @@ from dotenv import load_dotenv
 import requests ,json
 
 def get_data():
-    api_url = os.getenv('API_url')
-    response = requests.get(url=api_url)
-    data_str = response.text
-    data = json.loads(data_str)
-    response.raise_for_status()
-    
-    print(data)
-
-def main():
     load_dotenv()
-    get_data()
+    api_url = os.getenv('key')
+    response = requests.get(url=api_url)
+    return response.text
 
-if __name__ == '__main__':
-    main()
+def weather_condition(data):
+    pass
+
+
+get_data()
