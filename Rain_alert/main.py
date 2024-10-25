23646938a68db402ba11f5ff14dd684a6f875d32
Auston Mtabane <mtabane.auston@gmail.com>
2024-10-25 21:08:49 +0200

    comments

--- Rain_alert/main.py
+++ Rain_alert/main.py
@@ -3,18 +3,19 @@ from dotenv import load_dotenv
 import requests ,json
 from twilio.rest import Client
 
-def get_data(location):
-    'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}'
+def get_data(location): #location is a list of size two, containin lattitude and longitude coordinates
+    
     load_dotenv()
     API_key = os.getenv('key')
     lat = location[0]
     lon = location[1]
     api_url =f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}' 
     response = requests.get(url=api_url)
-    data = json.loads(response.text)
+    data = json.loads(response.text)    'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}'
+
     return data['weather']
 
-def sms(content,to_number):
+def sms(content,to_number):# sends an sms to the to_number
     load_dotenv()
     account_sid = os.getenv('account_sid')
     auth_token = os.getenv('auth_token')
