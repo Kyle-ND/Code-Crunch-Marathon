efefeed434af5e9943407da3346aeeee84fd8287
FreakyJay1 <jjthebestmanmode@gmail.com>
2024-10-26 14:19:07 +0200

    nearly done just errors

---
+++ Rain_alert/main.py
@@ -0,0 +1,56 @@
+import requests
+import geocoder
+from dotenv import load_dotenv
+from twilio.rest import Client
+import os
+
+load_dotenv()
+# fecthing everything from my .env
+SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_URL = os.getenv("SECRET_URL")
+API_KEY = os.getenv("API_KEY")
+API_URL = os.getenv("API_URL")
+ACCOUNT_SID = os.getenv("ACCOUNT_SID")
+AUTH_TOKEN = os.getenv("AUTH_TOKEN")
+MESSAGING_SERVICE_SID = os.getenv("MESSAGING_SERVICE_SID")
+# To fecth user City so i can use it to get the weather info on that city and it is for my device only.
+def get_city():
+    g = geocoder.ip('me')
+    if g.city is not None:
+        return g.city
+    else:
+        return None
+# create a function to fecth data from the that user city we just got now
+def get_data():
+    city = get_city()
+    completed_url = API_URL + "appid=" + API_KEY + "&q=" + city
+    response = requests.get(completed_url)
+    info = response.json()
+    return info
+# create a function to get name of city, maximum temp, minimum temp, main of the weather and the description of the weather main.
+def result():
+    info = get_data()
+    if info["cod"] != 404:
+        city_name = info['name']
+        x = info['main']
+        maximum_temp = round(x['temp_max'] - 273.15)
+        minimum_temp = round(x['temp_min'] - 273.15)
+        y = info['weather']
+        main_weather = y[0]['main']
+        description = y[0]['description']
+        print(f' The city: {city_name} \n The maximun temperature in celsius: {maximum_temp}°C \n The minimun temperature in celsius: {minimum_temp}°C \n The main of the weather: {main_weather} \n The description: {description}')
+    else:
+        print(" City Not Found ")
+# Create the main function to send result to sms with twilio.
+def main():
+    content = result()
+    phone = '+27716467174'
+    client = Client(ACCOUNT_SID,AUTH_TOKEN)
+    message = client.messages.create(
+        MESSAGING_SERVICE_SID,
+        body = content,
+        to = phone)
+    return message
+# calling the main function.
+main()
+
---
+++ birthday_wisher/main.py
@@ -0,0 +1,57 @@
+import os
+import requests,json
+from datetime import datetime
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+# fecthing my secret_key and secret_url from my .env
+SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_URL = os.getenv("SECRET_URL")
+# create a function to fecth data from the secret_url
+def getdata():
+    response = requests.get(url=SECRET_URL)
+    text = response.text
+    data  = json.loads(text)
+    
+    
+    return data["sheet1"]
+
+# create the main function
+def email_birthday():
+    sender_email = "jjthebestmanmode@gmail.com"
+    today = datetime.today().strftime("%d / %m")
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.starttls()
+    server.login(sender_email,SECRET_KEY)
+    datas = getdata()
+    print(datas)
+    birthdaytoday = False
+    for info in datas:
+        receiver_email = info["email"]
+        fullname  = info["fullname"]
+        company_name = "Jason Arnold"
+        message = f"""
+        Happy Birthday {fullname}🎉🎂
+        May your day be filled with joy, love, and laughter. Wishing you a year ahead filled with blessings, success, and happiness. 
+        May God bless you abundantly today and always! 🙏✨
+        
+        Best Regards
+        {company_name}
+        """
+        if info["birthday"] == today:
+            msg = MIMEMultipart("body")
+            msg["Subject"] = f'Happy Birthday {fullname}🎉🎂'
+            msg["From"] = sender_email
+            msg["To"] = receiver_email
+            server.sendmail(sender_email,receiver_email,message)
+            birthdaytoday = True
+    if birthdaytoday != True:
+        print("It is no one's birthday today")
+    
+    
+    server.quit()
+    
+email_birthday()
