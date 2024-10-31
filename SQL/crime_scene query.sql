f2f1729f06d1183fd68a270f9cb6611d6fa16197
Kamogelo Mmopane <101883751+KAMO333@users.noreply.github.com>
2024-10-31 15:37:05 +0200

    added files

---
+++ SQL/crime_scene query.sql
@@ -0,0 +1,4 @@
+-- Active: 1730372726448@@127.0.0.1@3306
+SELECT * FROM crime_scene_reports WHERE day = 28 AND month = 7 AND year = 2023 AND street = 'Humphrey Street' LIMIT 100
+
+-- The crime took place at 10:15am at Humphrey Street bakery, with 3 witnessess.
---
+++ SQL/info_from_witnessess.sql
@@ -0,0 +1,4 @@
+-- Active: 1730372726448@@127.0.0.1@3306
+SELECT * FROM interviews WHERE day = 28 AND month = 7 AND year = 2023 
+
+-- from witness statement I get: 1. Call less than 60 sec. 2. flight the following morning. 3. Flight tickets. 4. cars leaving parking lot around the same time. 4. ATM transactions on Legget street.
