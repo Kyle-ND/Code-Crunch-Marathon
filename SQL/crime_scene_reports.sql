89d5c6b27551c601563bf6df832f1a6ab3997888
Dimakyi <masakecharlotte@gmail.com>
2024-11-01 16:02:18 +0200

    done

--- SQL/crime_scene_reports.sql
+++ SQL/crime_scene_reports.sql
@@ -1,61 +1,22 @@
-SELECT * 
-FROM crime_scene_reports
-WHERE day = 28
-AND month = 7
-AND year = 2023
-AND street = 'Humphrey Street'
-AND description LIKE '%CS50%';
+select * from people
+INNER JOIN phone_calls on people.phone_number = phone_calls.caller
+INNER join bakery_security_logs on bakery_security_logs.license_plate = people.license_plate
+INNER JOIN passengers on passengers.passport_number = people.passport_number
+INNER join bank_accounts on bank_accounts.person_id = people.id
+INNER join atm_transactions on atm_transactions.account_number = bank_accounts.account_number
+inner join flights on passengers.flight_id = flights.id
+inner join airports on airports.id = flights.origin_airport_id
+where phone_calls.day = 28 and phone_calls.month = 7
+and phone_calls.duration < 60
+and bakery_security_logs.activity like '%exit%'
+and bakery_security_logs.month = 7 and bakery_security_logs.day = 28
+and bakery_security_logs.hour = 10 and bakery_security_logs.minute BETWEEN 15 and 25
+and atm_transactions.transaction_type like '%withdraw%' and atm_transactions.day = 28
+and airports.full_name like '%fiftyville%' and flights.day = 29 and flights.hour < 12
 
-SELECT * FROM interviews 
-WHERE day = 28
-AND month = 7
-AND year = 2023
-And transcript LIKE '%bakery%';
 
-SELECT * FROM bakery_security_logs 
-WHERE year = 2023
-AND day = 28
-AND month = 7
-AND activity = 'exit'
-AND hour LIKE '10'
-AND minute BETWEEN 15 AND 25;
 
-SELECT * FROM phone_calls
-WHERE year = 2023
-AND day = 28
-AND month = 7
-AND duration < 60;
-(
-    SELECT * FROM people 
-    EXCEPT 
-    SELECT * FROM bakery_security_logs
-)   INTERSECT (
-    SELECT * FROM people 
-    EXCEPT
-    SELECT * FROM bakery_security_logs
-);
 
-SELECT * from atm_transactions
-WHERE year = 2023
-AND day = 28
-AND month = 7
-AND transaction_type LIKE '%withdraw%'
-AND atm_location LIKE 'leggett Street';
-
- 
-SELECT * FROM airports
-WHERE city LIKE '%Fiftyville';
-
-SELECT * FROM flights
-WHERE year = 2023
-AND day = 29
-AND month = 7
-AND origin_airport_id = 8
-AND hour BETWEEN 8 AND 12;
-
-SELECT * FROM passengers 
-WHERE flight_id = 8
-AND passport_number
 
 
 
