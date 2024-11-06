6f92feae940cf088475bbebc435416540d2abcb9
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-11-06 21:12:58 +0200

    Add try-except block when making a request, and add test case.

--- Rain_alert/main.py
+++ Rain_alert/main.py
@@ -4,9 +4,10 @@ A module to check rain forecast and arn the user through Twilio SMS
 
 from creds import api_key
 import requests
+from requests.exceptions import HTTPError
 
 
-def get_weather_data(lat: float, lon: float) -> None:
+def get_weather_data(lat: float, lon: float, exclude: str) -> dict:
     """
     Get current weather from the given city's latitude and longitude.
 
@@ -16,6 +17,27 @@ def get_weather_data(lat: float, lon: float) -> None:
     Return:
         weather_data (dict): All weather information about the city
     """
-    URL = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={api_key}"
 
-    response = requests.get(URL)
+    URL = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={exclude}&appid={api_key}"
+    test_url = "https://api.github.com"
+    #f"api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=11169032316a0187407b8f2feec650e9"
+    
+    # Catch any errors that may arise trying to GET the Weather data
+    try:
+        response = requests.get(URL)
+        response.raise_for_status()
+    except HTTPError as http_err:
+        print(f"HTTP Error occured: {http_err}")
+    except Exception as e:
+        print(f"Other error occured: {e}")
+    else:
+        return response.json() # If request was a success
+
+
+if __name__ == "__main__":
+    # Test data
+    # Johannesburg (lat, lon)
+    lat = -26.204103
+    lon = 28.047304
+    exclude = "hourly"
+    print(get_weather_data(lat, lon, exclude))
