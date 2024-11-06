cd46b381b2644276c4d8e5009d423f2ccd481015
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-11-06 21:49:35 +0200

    Retrieve user's current location and use that for getting current weather.

--- Rain_alert/main.py
+++ Rain_alert/main.py
@@ -20,7 +20,7 @@ def get_weather_data(lat: float, lon: float, exclude: str) -> dict:
 
     URL = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={exclude}&appid={api_key}"
     test_url = "https://api.github.com"
-    
+
     #f"api.openweathermap.org/data/2.5/weather?q=London,uk&APPID={api_key}"
     
     # Catch any errors that may arise trying to GET the Weather data
@@ -35,10 +35,37 @@ def get_weather_data(lat: float, lon: float, exclude: str) -> dict:
         return response.json() # If request was a success
 
 
+def get_user_location() -> tuple[str, str]:
+    """
+    Get current user location and return coordinates (lat, lon)
+
+    Parameters:
+        None
+    Return:
+        location (tuple): Location of user in latitude and longitude
+    """
+
+    url = "https://ipinfo.io/"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except HTTPError as http_err:
+        print(f"HTTP Error occured: {http_err}")
+    except Exception as e:
+        print(f"Other error occured: {e}")
+    else:
+        json_format = response.json() # Make response into json for referencing
+        location_str = tuple(json_format['loc'].split(",")) # Get lat, long, make them a tuple
+        # Convert the str location to a float and give it back
+        return tuple(map(float, location_str)) 
+
+
 if __name__ == "__main__":
     # Test data
     # Johannesburg (lat, lon)
     lat = -26.204103
     lon = 28.047304
     exclude = "hourly"
-    print(get_weather_data(lat, lon, exclude))
+    print(f"Hardcoded: ({lat}, {lon})")
+    print(f"Retrieval: {get_user_location()}")
+    # print(get_weather_data(lat, lon, exclude))
