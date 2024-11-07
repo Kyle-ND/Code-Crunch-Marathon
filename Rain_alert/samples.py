6e8ab04b7207ddb17d1ef33b14f2d96ddf7d65bb
Sakhile Ndlazi <sandlazjhb024@student.wethinkcode.co.za>
2024-11-07 10:56:28 +0200

    Add twilio sample response with unverified number.

--- Rain_alert/samples.py
+++ Rain_alert/samples.py
@@ -1,3 +1,4 @@
+# Sample response from Openweather API
 wx_sample_response =  {
     "lat": 52.2297,
     "lon": 21.0122,
@@ -52,4 +53,13 @@ sample_dict_wx_data = {
         "icon": "01d"
         }
     ]   
+}
+
+# Sample response from Twilio
+400 - BAD REQUEST - The data given in the POST or PUT failed validation. Inspect the response body for details.
+{
+  "code": 21608,
+  "message": "The number +27082581XXXX is unverified. Trial accounts cannot send messages to unverified numbers; verify +27082581XXXX at twilio.com/user/account/phone-numbers/verified, or purchase a Twilio number to send messages to unverified numbers",
+  "more_info": "https://www.twilio.com/docs/errors/21608",
+  "status": 400
 }
