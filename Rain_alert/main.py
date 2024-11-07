ef50ccc57875f7183e698fcab7c1c0b4073333e1
Sakhile Ndlazi <sandlazjhb024@student.wethinkcode.co.za>
2024-11-07 11:27:08 +0200

    Use process_message from the weather API to send user warning,

--- Rain_alert/main.py
+++ Rain_alert/main.py
@@ -5,7 +5,8 @@ A module to check rain forecast and arn the user through Twilio SMS
 from helper import (
     get_user_location,
     get_weather_data,
-    send_sms
+    send_sms,
+    process_weather
 )
 
 
@@ -26,9 +27,9 @@ def main() -> None:
     exclude = 'hourly'
     hourly_forcast = get_weather_data(lat, lon, exclude)
     # Process that data and formulate warning for the user
-    mock_warning = "Rain in the next hour. We cannot sell ice cream today."
+    warning = process_weather(hourly_forcast)
     # Send the user a warning SMS
-    send_sms(mock_warning)
+    send_sms(warning)
 
 
 if __name__ == "__main__":
